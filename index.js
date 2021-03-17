ecee78f410f844ce5df2d8c6d875a6ca0a31e9a1
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-03-17 21:40:09 +0000

    Fix API invalid authorization header issue

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let api = "Basic 10945ecb-a140-4caf-b4e7-30a67ba2312d";
+let api = "d2a36ad2-bc98-45c0-a119-97bee7b4e679:";
+let encodedString = btoa(api);
 
-fetch("http://api.companieshouse.gov.uk/search/companies?q=test", {
+fetch("https://api.company-information.service.gov.uk/company/00000006", {
+  method: "GET",
   headers: {
-    Authorization: api,
+    "Authorization": "Basic " + encodedString,
     "Content-Type": "application/json",
   },
 })
   .then((response) => response.json())
   .then((json) => console.log(json))
   .catch((error) => console.log(error));
-
-
-//   console.log('bello')
