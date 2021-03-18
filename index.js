a449cc133552f458b3da65d5a0d59de89a849e2f
designaky <designaky@gmail.com>
2021-03-18 14:50:10 +0000

    Manipulate innerHtml of figure by adding a new img with a src from the Image API
    Closes #6

--- index.js
+++ index.js
@@ -35,28 +35,76 @@ fetch("https://api.company-information.service.gov.uk/company/00000006", {
 //   .catch((error) => console.log(error));
 
 
-  //Image API
-  const pexelApi = '563492ad6f91700001000001bf9128825e32458bbc14804fc4881c1d'
-  const searchTerm = 'panda'
+// Company names API
 
-   fetch(`https://api.pexels.com/v1/search?query=${searchTerm}&per_page=80`, {
-    method: 'GET',
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const companyName = document.querySelector("#name").value.toUpperCase();
+
+  let api = "d2a36ad2-bc98-45c0-a119-97bee7b4e679:";
+  let encodedString = btoa(api);
+  let proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  // let searchQuery = "FAC"
+  let url =
+    proxyUrl +
+    `https://api.companieshouse.gov.uk/search/companies?q=${companyName}`; /* Test is the query from the input field */
+
+  fetch(url, {
+    method: "GET",
     headers: {
-      "Authorization": pexelApi,
+      Authorization: "Basic " + encodedString,
       "Content-Type": "application/json",
     },
   })
-   .then((response) => response.json())
-   .then((json) => getRandomPhoto(json.photos))
-   .catch((error) => console.log(error))
-   
-   //Data base manipulation
+    .then((response) => response.json())
+    .then((json) => {
+      // console.log(json.items)
+      let result = json.items.filter((companyObj) => {
+        return companyObj.title.includes(companyName);
+      });
+      console.log(result);
+    })
+    .catch((error) => console.log(error));
+});
 
-   //Get random image
-   const getRandomPhoto = (photos) => {
-     const randomNumber = Math.floor(Math.random() * photos.length)
-     const randomImageUrl = photos[randomNumber].src.medium
-    //  console.log(randomImageUrl)
-     return randomImageUrl
-    }
+//Image API
+const pexelApi = "563492ad6f91700001000001bf9128825e32458bbc14804fc4881c1d";
+const searchTerm = "panda";
+const generateLogoBtn = document.getElementById("btn__generate");
+const outputFigure = document.querySelector("figure");
 
+//Data base manipulation
+
+//Get random image
+const getRandomPhoto = (photos) => {
+  const randomNumber = Math.floor(Math.random() * photos.length);
+  const randomImageUrl = photos[randomNumber].src.medium;
+  // console.log("Function getRandomPhoto", randomImageUrl);
+  return randomImageUrl;
+};
+
+const changeImageSrc = (imageUrl) => {
+  outputFigure.innerHTML = `<img src="${imageUrl}" alt="" />`;
+};
+
+const generateLogo = () => {
+  // Get the random photo
+
+  fetch(`https://api.pexels.com/v1/search?query=${searchTerm}&per_page=80`, {
+    method: "GET",
+    headers: {
+      Authorization: pexelApi,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => getRandomPhoto(json.photos))
+    .then((imageUrl) => changeImageSrc(imageUrl))
+    .catch((error) => console.log(error));
+
+  //
+};
+
+generateLogoBtn.addEventListener("click", generateLogo);
